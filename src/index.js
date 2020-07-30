d0ffbc9cf62b47a1345b6122f3548b4be6070d95
Jun-cloud <kjy8827@gmail.com>
2020-07-30 22:04:19 +0900

    Redux - reducer and store

--- src/index.js
+++ src/index.js
@@ -1,7 +1,17 @@
+import {createStore} from "redux";
+
 const add = document.getElementById("add");
 const minus = document.getElementById("minus");
 const number = document.querySelector("span");
 
+// reducer와 store는 개념적 용어이기 때문에 꼭 변수이름을 reducer, store로 할 필요는 없음
+// reducer : data를 변경하기 위한 fucntion
+const reducer = () => {}
+
+// store : data를 저장하기 위한 저장소
+const store = createStore(reducer);
+
+/*
 let count = 0;
 number.innerText = count;
 
@@ -21,3 +31,4 @@ const handleMinus = () => {
 
 add.addEventListener("click", handleAdd);
 minus.addEventListener("click", handleMinus);
+*/
